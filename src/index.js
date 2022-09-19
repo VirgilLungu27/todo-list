ddf61c7f61b5f9bc464f3c70d5069daa9f5bea7f
Virgil <lunguvirgil3000@gmail.com>
2022-09-19 12:57:58 +0300

    launch live server and create test todo list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+import _ from 'lodash';
+import './style.css';
+
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = 
+    `<div class="hero">
+        <div class="todo">
+            <div><h3>Todo List</h3></div>
+            
+            <div><h4>Buy cat food</h4></div>
+            <div><h4>Play games</h4></div>
+            <div><h4>Learn JavaScript</h4></div>
+            <div><h4>Listen to heavy metal</h4></div>
+            <div><h4>Learn Python</h4></div>
+        </div>
+    </div>
+    `
   
     return element;
   }
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
