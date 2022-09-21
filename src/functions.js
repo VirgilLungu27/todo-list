d96dcdbece93cb2df763f9fa1841ec091dc5caf7
Virgil <lunguvirgil3000@gmail.com>
2022-09-21 10:48:42 +0300

    Added test functionality and back button to all functions in the single js file

--- src/functions.js
+++ src/functions.js
@@ -51,8 +51,23 @@ function notesLoad() {
     const element = document.createElement('div');
     element.className = 'content';
     element.innerHTML = `
-    <div>
-    This is a test for the notes page.</div>
+    <div class="hero">
+        <div class="main">
+            <h3>Find your project notes below.</h3>
+        </div>
+        <button id="back">Go back</button>
+
+        <div class="todo">
+            <div class="todofirst"><h3>Todo List</h3></div>
+        
+            <div id="homepage"><h4>Home</h4></div>
+            <div id="today"><h4>Today</h4></div>
+            <div id="tomorrow"><h4>Tomorrow</h4></div>
+            <div id="thisweek"><h4>This week</h4></div>
+            <div id="projects"><h4>Projects</h4></div>
+            <div id="notes"><h4>Notes</h4></div>
+        </div>
+    </div>
     `
     return element;
 }
@@ -61,8 +76,23 @@ function projectsLoad() {
     const element = document.createElement('div');
     element.className = 'content';
     element.innerHTML = `
-    <div>
-    This is a test for the projects page.</div>
+    <div class="hero">
+        <div class="main">
+            <h3>Find all of your projects and project ideas below.</h3>
+        </div>
+        <button id="back">Go back</button>
+
+        <div class="todo">
+            <div class="todofirst"><h3>Todo List</h3></div>
+        
+            <div id="homepage"><h4>Home</h4></div>
+            <div id="today"><h4>Today</h4></div>
+            <div id="tomorrow"><h4>Tomorrow</h4></div>
+            <div id="thisweek"><h4>This week</h4></div>
+            <div id="projects"><h4>Projects</h4></div>
+            <div id="notes"><h4>Notes</h4></div>
+        </div>
+    </div>
     `
     return element;
 }
@@ -71,8 +101,23 @@ function thisWeekLoad() {
     const element = document.createElement('div');
     element.className = 'content';
     element.innerHTML = `
-    <div>
-    This is a test for this week page.</div>
+    <div class="hero">
+        <div class="main">
+            <h3>Find your tasks for this week below.</h3>
+        </div>
+        <button id="back">Go back</button>
+
+        <div class="todo">
+            <div class="todofirst"><h3>Todo List</h3></div>
+        
+            <div id="homepage"><h4>Home</h4></div>
+            <div id="today"><h4>Today</h4></div>
+            <div id="tomorrow"><h4>Tomorrow</h4></div>
+            <div id="thisweek"><h4>This week</h4></div>
+            <div id="projects"><h4>Projects</h4></div>
+            <div id="notes"><h4>Notes</h4></div>
+        </div>
+    </div>
     `
     return element;
 }
@@ -81,8 +126,23 @@ function tomorrowLoad() {
     const element = document.createElement('div');
     element.className = 'content';
     element.innerHTML = `
-    <div>
-    This is a test for the tomorrow page.</div>
+    <div class="hero">
+        <div class="main">
+            <h3>Find your todo list for tomorrow down below.</h3>
+        </div>
+        <button id="back">Go back</button>
+
+        <div class="todo">
+            <div class="todofirst"><h3>Todo List</h3></div>
+        
+            <div id="homepage"><h4>Home</h4></div>
+            <div id="today"><h4>Today</h4></div>
+            <div id="tomorrow"><h4>Tomorrow</h4></div>
+            <div id="thisweek"><h4>This week</h4></div>
+            <div id="projects"><h4>Projects</h4></div>
+            <div id="notes"><h4>Notes</h4></div>
+        </div>
+    </div>
     `
     return element;
 }
